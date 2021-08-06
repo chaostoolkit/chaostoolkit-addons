235069fb10bd1e890ec50937f60b251afd64c943
Sylvain Hellegouarch <sh@defuze.org>
2021-08-06 11:40:46 +0200

    Use external deps on older version
    Signed-off-by: Sylvain Hellegouarch <sh@defuze.org>

--- chaosaddons/__init__.py
+++ chaosaddons/__init__.py
@@ -1,4 +1,7 @@
-from importlib.metadata import version, PackageNotFoundError
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:
+    from importlib_metadata import version, PackageNotFoundError
 
 __all__ = ["__version__"]
 
